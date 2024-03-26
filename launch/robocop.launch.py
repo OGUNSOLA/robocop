ea68033d4fd2e8d097225619f5d4997659b8b7a0
ogunsola <olubukola@me.com>
2024-03-25 21:32:34 -0400

    added navigation config file

--- launch/robocop.launch.py
+++ launch/robocop.launch.py
@@ -32,6 +32,12 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false'}.items()
     )
 
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )])
+    )
+
     controller_manager=  Node (
         package="controller_manager",
         executable="ros2_control_node",
@@ -70,6 +76,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        #joystick,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_broad_spawner,
